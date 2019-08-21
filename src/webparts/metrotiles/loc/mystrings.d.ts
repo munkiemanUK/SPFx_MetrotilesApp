185e063c13e2fe6acdb677dff2a3368cccbce0a7
Jason Clark <jclark@maximusuk.co.uk>
2019-08-21 11:38:51 +0100

    added project structure

---
+++ src/webparts/metrotiles/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IMetrotilesWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'MetrotilesWebPartStrings' {
+  const strings: IMetrotilesWebPartStrings;
+  export = strings;
+}
